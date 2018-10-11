7d3034aab2d199a7a86a937eebd702bc59d44526
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-11 13:38:16 +0200

    fin

---
+++ js.js
@@ -0,0 +1,11 @@
+// On créé la fonction d'affichage
+afficher.addEventListener("click", function(){
+//si on clique sur afficher on set visibility
+  text.style.display = "block";
+});
+
+// On fait la même chose pour le cacher
+none.addEventListener("click", function(){
+// On le cache
+  text.style.display = "none";
+});
